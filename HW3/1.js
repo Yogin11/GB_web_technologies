1edd057074756846d698420175cff29962fdb498
MaxYogin <yogin11@yahoo.com>
2023-02-21 19:11:47 +0300

    HW3 some simple tasks(corrected)

--- HW3/1.js
+++ HW3/1.js
@@ -1,7 +1,7 @@
 function convertToFahrenheit() {
     const t = Number.parseFloat(prompt("Введите температуру в градусах Цельсия"));
     let fahr = 9 / 5 * t + 32;
-    alert(`Температура в градусах Фаренгейта равна ${+fahr.toFixed(1)}`);
+    alert(`Цельсий: ${+t},  Фаренгейт: ${+fahr.toFixed(1)}`);
 }
 
 convertToFahrenheit();
